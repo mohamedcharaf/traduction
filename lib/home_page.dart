dcb517713524482add80cecd569d1fc957c3ea4f
mohamedcharaf <charafdaho.46@gmail.com>
2024-08-27 18:53:41 +0100

    first commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,13 +1,7 @@
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
-import 'package:trad/applocalistions.dart';
-
-import 'Setting_page.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-
   final String title;
 
   @override
@@ -15,62 +9,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-  
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.home),
-          onPressed: () {
-            
-          },
-         
-        ),
-        title: Text("settings".tr(context)), // Assuming 'tr' is a localization function
-        actions: [
-          IconButton(
-            icon: Icon(Icons.settings),
-            onPressed: () {
-               Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => SettingsPage()),
-              );
-            },
-          ),
-        ]
-
+        title: Text(widget.title),
       ),
       body: Center(
-  
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-         Text(
-             "helloText".tr(context),
-            ),
-            Text(
-              '$_counter',
-              // style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+        child: Text('Welcome to ${widget.title}'),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:trad/cubit/locale_cubit.dart';
-import 'applocalistions.dart';
 import 'home_page.dart';
 
 void main() {
@@ -14,45 +10,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => LocaleCubit()..getSavedLangage(),
-        ),
-      ],
-      child: BlocBuilder<LocaleCubit,  ChangeLocaleState>(
-        builder: (context, state) {
-       
-            return MaterialApp(
-              locale: state.locale,
-              supportedLocales: const [
-                Locale('en'),
-                Locale('ar'),
-              ],
-              localizationsDelegates: [
-                AppLocalizations.delegate,
-                GlobalMaterialLocalizations.delegate,
-                GlobalWidgetsLocalizations.delegate,
-                GlobalCupertinoLocalizations.delegate,
-              ],
-              localeResolutionCallback: (locale, supportedLocales) {
-                if (locale != null && supportedLocales.contains(locale)) {
-                  return locale;
-                }
-                return supportedLocales.first;
-              },
-              title: 'Flutter Demo',
-              theme: ThemeData(
-                primarySwatch: Colors.blue,
-              ),
-              home: const MyHomePage(title: 'Flutter Demo Home Page'),
-            );
-          }
-
-          // Show a loading indicator while waiting for locale to be resolved
-      
-   
+    return MaterialApp(
+      title: 'Your App Title',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: const MyHomePage(title: 'test'),
     );
   }
 }
